9c31d7a9606d5f2ff254d691a0ddc78145767531
saiteja0804 <99531351+saiteja0804@users.noreply.github.com>
2022-06-22 20:19:06 +0530

    Add files via upload

---
+++ patern_modules.js
@@ -0,0 +1,4 @@
+var stuff = require('./stuff')
+
+console.log(stuff.counter(['sai','Teja','kst']));
+console.log(stuff.adder(2,3));
---
+++ stuff.js
@@ -0,0 +1,10 @@
+var counter=function(arr){
+    return 'There are '+ arr.length + ' elements in the array'
+};
+    var adder = function(a,b)
+    {
+        return `The sum of 2 numbers is ${a+b}`
+    };
+
+module.exports.counter=counter;
+module.exports.adder=adder;
