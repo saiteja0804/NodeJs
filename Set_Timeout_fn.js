06e45cd98eaad08699ca9047733aba06cf2ff67b
saiteja0804 <99531351+saiteja0804@users.noreply.github.com>
2022-06-22 11:28:58 +0530

    Add files via upload

---
+++ Set_Timeout_fn.js
@@ -0,0 +1,5 @@
+//print the o/p after 5 sec
+
+setTimeout(function(){
+    console.log("Print after 5 sec");
+},5000)
---
+++ functn.js
@@ -0,0 +1,13 @@
+//function statement
+
+function call(){
+    console.log("fn call")
+   }
+   call();
+//function expression
+
+   var tata = function(){
+       console.log('bye')
+   }
+   
+   callFunction(tata);
