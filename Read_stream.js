e273336e0f85cdf95f693e70a2f9d4bd1f14e4c7
saiteja0804 <99531351+saiteja0804@users.noreply.github.com>
2022-06-25 11:23:36 +0530

    Add files via upload

---
+++ Read_stream.js
@@ -0,0 +1,10 @@
+var http=require('http');
+var fs= require('fs');
+
+var myReadStream=fs.createReadStream(__dirname +'/Readme.txt');
+
+myReadStream.on('data',function(chunk)
+{
+    console.log('new chunk recieved');
+    console.log(chunk);
+});
